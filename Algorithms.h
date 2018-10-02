4cc5699b740db2b6f574cf4d39f819ab33976dce
ank47197 <43782942+ank47197@users.noreply.github.com>
2018-10-02 20:52:48 +0530

    Update Algorithms.h
    Add counting sort

--- Algorithms.h
+++ Algorithms.h
@@ -185,4 +185,29 @@ void heap_sort(int arr[], int n)
 
 //=================
 
+//=========Counting Sort=========
+    void counting_sort(int A[], int Aux[], int sortedA[], int N) 
+{    // First, find the maximum value in A[]
+    int K = 0,j=0,i;
+    for(i=0; i<N; i++)
+        K = max(K, A[i]);
+ // Initialize the elements of Aux[] with 0
+    for(i=0 ; i<=K; i++) 
+        Aux[i] = 0;
+    // Store the frequencies of each distinct element of A[],
+    // by mapping its value as the index of Aux[] array
+    for(i=0; i<N; i++)
+        Aux[A[i]]++;
+    for(i=0; i<=K; i++) {
+        int tmp = Aux[i];      // Aux stores which element occurs how many times,
+        // Add i in sortedA[] according to the number of times i occured in A[]
+        while(tmp--) {
+            sortedA[j] = i;
+            j++;
+        }
+    }
+}
+
+//=========================
+
 #endif // ALGORITHMS_H_INCLUDED
